3fb2ced0b21e37f949fc152ff0c3504a2ee9672a
cooper-anderson <cooper@cooperstuff.ninja>
2016-09-10 00:43:56 -0700

    Indev v0.1.54: Started new branch: reworked-all

--- app/js/blockade.js
+++ app/js/blockade.js
@@ -1,87 +1,114 @@
-/*** Created by Cooper Anderson on 8/24/16 AD. ***/
-
-const uneval = require("uneval");
-
-class Vector {
-	constructor(x=0, y=0) {
-		this.x = x;
-		this.y = y;
-	}
-}
-
-class Object2 {
-	constructor(position=new Vector(), passable=false) {
-		this.position = position;
-		this.passable = passable
-	}
-}
-
-class Tile extends Object2 {
-	constructor(position=new Vector()) {
-		super(position, true);
-	}
-	SlideOver(block) {
-
-	}
-}
-
-class Plate extends Object2 {
-	constructor(position=new Vector()) {
-		super(position, true);
-	}
-}
-
-class Block extends Object2 {
-	constructor(position=new Vector()) {
-		super(position, false);
-		this.velocity = new Vector();
-		this.moveable = true;
-	}
-	Move() {
-		var space = new Vector(this.position.x + this.velocity.x, this.position.y + this.velocity.y);
-		while (typeof grid[space.x] == "object" && typeof grid[space.x][space.y] == "object" && typeof grid[space.x][space.y].tile == "object") {
-			grid[this.position.x][this.position.y].tile.SlideOver(this);
-		}
-	}
-}
-
-var tile = new Tile(new Vector(1, 1));
-var plate = new Plate(new Vector(1, 1));
-var block = new Block(new Vector(1, 1));
-
-
+/**
+ * Created by Cooper Anderson on 8/24/16 AD.
+ */
 
 /*
-$("#game").on("click", ".directional-button", function() {
-	if (!$(this).attr("disabled")) {
-		move();
-		id = $(this).data("id");
-		direction = $(this).attr("id");
-		directions = {"up": [0, -1], "down": [0, 1], "left": [-1, 0], "right": [1, 0]};
-		dir = {x: directions[direction][0], y: directions[direction][1]};
-		block = {x: leveldata.blocks[id][0], y: leveldata.blocks[id][1]};
-		count = 0;
-		while (grid[block.x + dir.x] instanceof Array && grid[block.x + dir.x][block.y + dir.y] instanceof Array && grid[block.x + dir.x][block.y + dir.y].includes("tile") && (!grid[block.x + dir.x][block.y + dir.y].includes("block"))) {
-			for(var i = 0; i < grid[block.x][block.y].length; i++) {
-				if(grid[block.x][block.y][i] == "block") {
-					grid[block.x][block.y].splice(i, 1);
-				}
-			}
-			block.x += dir.x; block.y += dir.y;
-			leveldata.blocks[id] = [block.x, block.y];
-			grid[block.x][block.y].push("block");
-		}
-		$("span:last-child", $(".block-group")).remove();
-		$(`.block-group#block-${id}`).animate({left: `${block.x * 50}px`, top: `${block.y * 50}px`}, 400, "swing", checkForWin);
-	}
-});
-*/
+ data.grid = {
+ 0: {
+ 0: {
+ tile: new StandardTile(new Vector2(0, 0))
+ },
+ 1: {
+ tile: new StandardTile(new Vector2(0, 1))
+ },
+ 2: {
+ tile: new StandardTile(new Vector2(0, 2)),
+ block: new StandardBlock(new Vector2(0, 2))
+ }
+ },
+ 1: {
+ 0: {
+ tile: new StandardTile(new Vector2(1, 0))
+ },
+ 1: {
+ tile: new StandardTile(new Vector2(1, 1)),
+ plate: new StandardPlate(new Vector2(1, 1))
+ },
+ 2: {
+ tile: new StandardTile(new Vector2(1, 2))
+ }
+ },
+ 2: {
+ 0: {
+ tile: new StandardTile(new Vector2(2, 0))
+ },
+ 1: {
+ tile: new StandardTile(new Vector2(2, 1))
+ },
+ 2: {
+ tile: new StandardTile(new Vector2(2, 2))
+ }
+ }
+ }
+ */
 
 
-function init(_data = '{"grid":{"0":{"0":{},"1":{},"2":{}},"1":{"0":{},"1":{"block":{"position":{"x":1,"y":2},"passable":false,"velocity":{"x":0,"y":0},"moveable":true},"plate":{"position":{"x":1,"y":2},"passable":true},"tile":{"position":{"x":1,"y":2},"passable":true}},"2":{}},"2":{"0":{},"1":{},"2":{}}},"creator":"Cooper Anderson","par":0}') {
+const {Vector2} = require("Vectors");
+const {Tile, StandardTile} = require("Tiles");
+const {Plate, StandardPlate} = require("Plates");
+const {Block, StandardBlock} = require("Blocks");
+
+var tile = new Tile(new Vector2(1, 1));
+var plate = new Plate(new Vector2(1, 1));
+var block = new StandardBlock(new Vector2(1, 1));
+
+var data;
+
+function init(_data = `{"grid":{"0":{"0":{"tile":{"position":{"x":0,"y":0},"color":"white","passable":true,"class":"StandardTile","objectType":"Tile"}},"1":{"tile":{"position":{"x":0,"y":1},"color":"white","passable":true,"class":"StandardTile","objectType":"Tile"}},"2":{"tile":{"position":{"x":0,"y":2},"color":"white","passable":true,"class":"StandardTile","objectType":"Tile"},"block":{"position":{"x":0,"y":2},"color":"white","passable":false,"velocity":{"x":0,"y":0},"moveable":true,"class":"StandardBlock","objectType":"Block"}}},"1":{"0":{"tile":{"position":{"x":1,"y":0},"color":"white","passable":true,"class":"StandardTile","objectType":"Tile"}},"1":{"tile":{"position":{"x":1,"y":1},"color":"white","passable":true,"class":"StandardTile","objectType":"Tile"},"plate":{"position":{"x":1,"y":1},"color":"white","passable":true,"active":false,"class":"StandardPlate","objectType":"Plate"}},"2":{"tile":{"position":{"x":1,"y":2},"color":"white","passable":true,"class":"StandardTile","objectType":"Tile"}}},"2":{"0":{"tile":{"position":{"x":2,"y":0},"color":"white","passable":true,"class":"StandardTile","objectType":"Tile"}},"1":{"tile":{"position":{"x":2,"y":1},"color":"white","passable":true,"class":"StandardTile","objectType":"Tile"}},"2":{"tile":{"position":{"x":2,"y":2},"color":"white","passable":true,"class":"StandardTile","objectType":"Tile"}}}},"name":"Debug","creators":["Cooper Anderson"],"par":0,"flags":[],"size":{"x":3,"y":3}}`) {
 	data = JSON.parse(_data);
-	grid = data.grid;
+	data.size = new Vector2();
+	for (var i in data.grid) {
+		data.size.x++;
+	}
+	for (var j in data.grid[0]) {
+		data.size.y++;
+	}
+}
 
-	grid[2][1].tile = tile;
-	block.velocity.x = 1;
+function printGrid() {
+	var grid = data.grid;
+	var list = []
+	var final = "";
+	for (var i in grid[0]) {
+		list.push("");
+	}
+	for (var x in grid) {
+		for (var y in grid[x]) {
+			char = ""
+			if (typeof grid[x][y].tile != "undefined") {
+				char = "t";
+			}
+			if (typeof grid[x][y].plate != "undefined") {
+				char = "p";
+			}
+			if (typeof grid[x][y].block != "undefined") {
+				char = "b";
+			}
+			list[y] += char;
+		}
+	}
+	for (var i = 0; i < list.length; i++) {
+		final += list[i] + "\n";
+	}
+	console.log(final);
+}
+
+//var pos, dir, dest, grid;
+
+function temp() {
+	pos = {x: 0, y: 2}
+	dir = {x: 1, y: 0}
+	dest = {x: 1, y: 2}
+	grid = data.grid;
+	isPassable = function(gridSpace) {
+		var passable = [];
+		for (key in gridSpace) {
+			passable.push(gridSpace[key].passable);
+		}
+		return !passable.includes(false);
+	};
+	printGrid();
+	data.grid[0][2].block.velocity.x = 1;
+	data.grid = eval(data.grid[0][2].block.class).Move(data.grid[0][2].block, data.grid)
+	printGrid();
 }
