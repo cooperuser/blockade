2de544ae5ae0a32302689435c0f3a9d0a772d475
Cooper Anderson <cooper@cooperstuff.ninja>
2019-08-10 09:51:59 -0700

    Remove init and final instances

--- source/scripts/js/overhaul/index.js
+++ source/scripts/js/overhaul/index.js
@@ -1,13 +1,13 @@
-const {Blockade, Vector2, Tiles, Blocks, Plates} = require("blockade");
+const {Blockade, Vector2, tiles, blocks, plates} = require("blockade");
 
 let b = new Blockade();
-let tile = b.addTile(Tiles.standard);
-let block = b.addBlock(Blocks.standard);
-b.addTile(Tiles.standard, new Vector2(1, 0));
-b.addTile(Tiles.standard, new Vector2(1, 1));
-b.addTile(Tiles.standard, new Vector2(2, 0));
-let plate = b.addPlate(Plates.standard, new Vector2(2, 0));
-b.addTile(Tiles.standard, new Vector2(2, 1));
-b.addTile(Tiles.standard, new Vector2(3, 0));
-//let block2 = b.addBlock(Blocks.standard, new Vector2(3, 0));
+let tile = b.addTile(tiles.Standard.type);
+let block = b.addBlock(blocks.Standard.type);
+b.addTile(tiles.Sticky.type, new Vector2(1, 0));
+b.addTile(tiles.Standard.type, new Vector2(1, 1));
+b.addTile(tiles.Standard.type, new Vector2(2, 0));
+let plate = b.addPlate(plates.Standard.type, new Vector2(2, 0));
+b.addTile(tiles.Standard.type, new Vector2(2, 1));
+b.addTile(tiles.Standard.type, new Vector2(3, 0));
+let block2 = b.addBlock(blocks.Standard.type, new Vector2(3, 0));
 b.render();
